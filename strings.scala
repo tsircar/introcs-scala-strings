745e90197cc2690ea90c104b628b39578dffbd12
trinas <tsircar@luc.edu>
2015-04-06 19:05:26 +0000

    finished lab

--- strings.scala
+++ strings.scala
@@ -1,47 +1,56 @@
-import scala.math.min
+ import scala.math.min
 
 object strings  {
-
-/*
-
-   Write a function that accepts a sentence (string), and returns whether
-   it represents a *declarative* sentence (i.e. ending in a period),
-   *interrogatory* sentence (ending in a question mark), or an
-   *exclamation* (ending in exclamation point) or is not a sentence
-   (anything else).
-
-   This may be the first time you write a conditional
-   statement. (This needs the next chapter.)
-   It makes sense to only make small changes at once and build
-   up to final code. First you might just code it to check if a sentence is
-   declarative or not. Then remember you can test further cases with
-   ``else if (...)``.
-
-   The function should return the string "interrogative", "declarative", or
-   "exclamatory" for ?, ., or !; otherwise, it should return "unknown".
- */
-
-
    def getSentenceType(sentence : String) : String = {
+    
+       
+                       if(sentence.endsWith(".")) { 
+                           "declarative"
+                                } 
+                        else if (sentence.endsWith("?")) {
+                        "interrogative"
+                        }
+                        else if (sentence.endsWith("!")){
+                        "exclamatory"
+                        }else{
+                        "unknown"
+                        }
+   }
+   
+ 
+
+
+//don't find space, return name, then return everything//
+//findthatoccurenceofaspace,positiolessthan0, position was not found in the name.find that occurence ofaspace.
+//-1 = not found 
+//last space position is less than 0
+//minus 1 returned but testing less than -
+//when something is found, it's suppose to be non-negative (0 + length-1)
+//not found, will be outside of that range
+//last space position less than 0, return name
+//DONT use return in scala
+//0 to last space position, everyting up until space character
+//last name. first name = glue the strings together "last name + ", " + " " + first name." 
+//Run the function in terminal: getFormattedName(enter Name) after sbt console, get nto colder and do ls and then once in folder, do "sbt console." 
+//val lastSpacePosition = name.lastIndex("  ")//
+//steps basically. 1) find the position of first space in name because the first space can indicate two things: if the person just has a name such as "Madonna" no last or middle name or if they do have a first and last name, and also to find out the position of where the space is.
+//also check position of last space to see if they have a middle or last name. Sstore positions of space and check for spaces using condition statements and .index of API. if a negative index indicates no space and a positive value 
+//2) finding the position of the first space and storing it allows use to create substrings. create a substring everything before the first space to get the first name and then create a substring from lastposition to get last name 
+//glue first and last name substrings together//
      
-      "unknown"
+                     def getFormattedName(name : String) : String = { 
+                            var fullname = ""
+                            var firstPos = name.indexOf(" ")
+                            var lastPos = name.lastIndexOf(" ")
+                            if (lastPos < 0) {
+                                fullname = name
+                            } 
+                         else {
+                            var first = name.substring(0, lastPos)
+                            var last = name.substring(lastPos+1)
+                                fullname =last + ", " +first
+                               }
+                                fullname
+                               }
    }
 
-/*
-   Given a name as input.  Assume first and last names
-   are separated by a space (no middle name).
-   Print last name first followed by a comma
-   and a space, followed by the first name.
-   For example, if the input is
-   ``"Marcel Proust"``, the output is ``"Proust, Marcel"``.
-
-   If the name doesn't have any spaces in it (i.e. it is a single
-   name, e.g. Madonna, return the name as is.
- */
-
-   def getFormattedName(name : String) : String = {
-     
-      "name not calculated yet"
-   }
-
-}
